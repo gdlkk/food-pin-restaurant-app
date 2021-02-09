2180f5b9df50735eca49b42aa6a7b41472188804
gdlkk <mateusz.d.mateusz@gmail.com>
2021-02-09 07:41:53 +0100

    Adding images, layout and some code

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Mateusz Dettlaff on 03/02/2021.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = 20.0
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
