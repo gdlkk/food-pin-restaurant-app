1184c5b4efb91e12aa3e2f57b7ab6dee16570e0c
gdlkk <mateusz.d.mateusz@gmail.com>
2021-02-19 18:16:05 +0100

    Expanding detail view controller, changing fonts

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -9,33 +9,55 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
     
-    @IBOutlet var restaurantImageView: UIImageView!
-    @IBOutlet var restaurantNameLabel: UILabel!
-    @IBOutlet var restaurantTypeLabel: UILabel!
-    @IBOutlet var restaurantLocationLabel: UILabel!
-
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
+    @IBOutlet var tableView: UITableView!
+    
     var restaurant = Restaurant()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        restaurantImageView.image = UIImage(named: restaurant.image)
-        restaurantNameLabel.text = restaurant.name
-        restaurantTypeLabel.text = restaurant.type
-        restaurantLocationLabel.text = restaurant.location
         
-        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        headerView.restaurantNameLabel.text = restaurant.name
+        headerView.restaurantTypeLabel.text = restaurant.type
+        headerView.restaurantImageView.image = UIImage(named: restaurant.image)
+        
+        let heartImage = restaurant.isFavorite ? "heart.fill" : "heart"
+        headerView.heartButton.tintColor = restaurant.isFavorite ? .systemYellow : .white
+        headerView.heartButton.setImage(UIImage(systemName: heartImage), for: .normal)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.separatorStyle = .none
+    }
+}
+
+extension RestaurantDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+                cell.descriptionLabel.text = restaurant.description
+                return cell
+            
+            case 1:
+                let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self), for: indexPath) as! RestaurantDetailTwoColumnCell
+                
+                cell.column1TitleLabel.text = "Address"
+                cell.column1TextLabel.text = restaurant.location
+                cell.column2TitleLabel.text = "Phone"
+                cell.column2TextLabel.text = restaurant.phone
+                
+                return cell
+            
+            default:
+                fatalError("Failed to instantiate the table view cell or detail view controller")
+        }
     }
-    */
-
 }
---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Mateusz Dettlaff on 19/02/2021.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+    
+    @IBOutlet var restaurantNameLabel: UILabel! {
+        didSet {
+            restaurantNameLabel.numberOfLines = 0
+            
+            if let customFont = UIFont(name: "Nunito-Bold", size: 40.0) {
+                restaurantNameLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: customFont)
+            }
+        }
+    }
+    @IBOutlet var restaurantTypeLabel: UILabel! {
+        didSet {
+            if let customFont = UIFont(name: "Nunito-Bold", size: 20.0) {
+                restaurantTypeLabel.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
+            }
+        }
+    }
+    @IBOutlet var restaurantImageView: UIImageView!
+    @IBOutlet var heartButton: UIButton!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
